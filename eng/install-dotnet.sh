ff701005965259634b0812009d158d94a0d09d28
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-11-16 08:28:26 +1100

    Set dotnet home path

--- eng/install-dotnet.sh
+++ eng/install-dotnet.sh
@@ -1,6 +1,4 @@
-export DOTNET_VERSION=6.0.100
 echo "Building Pyjion with .NET  $DOTNET_VERSION"
 yum install -y wget && yum clean all
-wget https://dotnetcli.azureedge.net/dotnet/Sdk/${DOTNET_VERSION}/dotnet-sdk-${DOTNET_VERSION}-linux-arm64.tar.gz
-mkdir -p dotnet && tar zxf dotnet-sdk-${DOTNET_VERSION}-linux-arm64.tar.gz -C /usr/local/share/dotnet
-export DOTNET_ROOT=/usr/local/share/dotnet
+wget -q https://dotnetcli.azureedge.net/dotnet/Sdk/${DOTNET_VERSION}/dotnet-sdk-${DOTNET_VERSION}-linux-arm64.tar.gz
+mkdir -p /tmp/dotnet && tar zxf dotnet-sdk-${DOTNET_VERSION}-linux-arm64.tar.gz -C /tmp/dotnet
